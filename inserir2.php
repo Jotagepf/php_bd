bc9fde2cd9799808fc8f9eb98b0cb67aa8238a8e
João Gabriel Portela <jgportela@gmail.com>
2024-09-09 15:15:26 -0300

    Trabalho

---
+++ inserir2.php
@@ -0,0 +1,19 @@
+<?php
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        // echo $nome."<br>";
+        // echo $email."<br>";
+        // echo $senha."<br>";
+        include "conecta.php";
+        $sql = "INSERT INTO usuario(nome, senha, email) values('$nome', '$senha', '$email')";
+        $resultado = mysqli_query($conexao, $sql);
+        if($resultado > 0)
+        {      
+                echo "Cadastro com sucesso!";
+                
+        }
+                
+
+            
+?>
---
+++ usuarios.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        ul{
+            display: flex;
+            list-style: none;
+            background-color: black;
+        }
+        li{
+            padding: 10px;
+        }
+        *{
+            margin:0;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+        }
+        a:hover{
+            background-color: white;
+            color: black;
+            padding: 8px;
+            transition: 250ms;
+
+        }
+
+        main{
+            display: flex;
+        }
+
+        img{
+            width: 70px;
+            
+        }
+        
+        
+        #dados{
+            width: "80%";
+            height:"80%";
+            color: green;
+            border: 2px solid;
+            border-radius: 2px solid;
+            background-color: red;
+            padding: 10px;
+
+        }
+        span{
+            color: black;
+            font-weight: bold ;
+            border-bottom: 1px solid;
+            margin: 2px;
+        }
+        p{
+            background-color: palegreen;
+            border:  1px dashed;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Contatos</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div id="imagem">
+        <img src="imagem.jpg">
+    </div>
+          <div id="dados">
+          
+
+        <p>
+        <?php
+            include "conecta.php";
+                
+                $sql = "SELECT id, nome, senha, email FROM usuario";
+                $resultado = mysqli_query($conexao, $sql);
+                echo "<br>";
+  
+                if (mysqli_num_rows($resultado) > 0){
+                while($registro = mysqli_fetch_assoc($resultado)) {
+                    echo "<span> ID: </span> ". $registro["id"];
+                    echo "<span> Nome: </span> ". $registro["nome"];
+                    echo "<span> E-mail: </span> ". $registro["email"];
+                    echo "<br>";
+                }
+
+            }
+            ?>
+            </p>
+        </div>
+    </main>
+</body>
+</html>
